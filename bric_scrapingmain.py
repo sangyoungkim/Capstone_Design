529648bab7c9faba683d144cc8590d5805a1610b
sangyoungkim <rky7429@gmail.com>
2022-05-27 23:58:10 +0900

    딱히 의미있는 커밋은 아님

--- bric_scrapingmain.py
+++
@@ -1,8 +0,0 @@
-import bric_scraping as bric
-
-# 브릭 대학원생 채용 정보 사이트에서 내가 원하느 정보만 필터링해서 보고 싶어서 만든 코드
-
-find_word =["생명공학","시스템","컴퓨터","정보","합성",]
-start=8
-bric.print_filtering_find_number(find_word,start)
-print("종료되었습니다.")
