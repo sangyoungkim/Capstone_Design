77f2fbebde0ae1c4e86ac719ba62973b7e0972f1
sangyoungkim <rky7429@gmail.com>
2022-06-12 18:02:20 +0900

    데이터 시각화 과제 완료

--- data_pandas_study.py
+++ data_pandas_study.py
@@ -1,25 +1,40 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
 import numpy as np
-#X축 기간 Y축 랭크 각 상품이 기간에 따라 랭크가 어떻게 변하는지
+#정문제 : data.csv X축 기간 Y축 랭크 각 상품이 기간에 따라 랭크가 어떻게 변하는지
 
-# def fuc_1(arr):
-#     #입력받은 데이터 프레임을 배열형태로 반환해주는 함수로 
-#     #데이터 프레임에 특정 한개의 colums만 매개변수로 입력해야한다 
-#     #또한 x축과 y축에 값으로 쓸 배열을 위한 함수로 배열내 중복값을 전부 제거해준ㄷ
+#1. 받은 데이터에서 필요한 날짜 이름 랭크정보만 남겨서 가공
+#2. 데이터 안에서 상품 이름만 가져와서 배열로 저장
+#3. 이름으로 데이터 조회해서 그 상품에 날짜별 랭크를 얻어냄
+#4. 배열 안에 있는 모든 상품 그래프 그려냄
 
-#     arr = arr.values#데이터 프레임을 배열형식으로 변환 (2차원 배열)
-#     rearr=[]
-#     for i in range(0,len(arr)):
-#         rearr.append(arr[i][0])
-#     #2차원 배열을 1차원 배열로 변경해줌
-#     rearr = set(rearr)
+#=======================데이터 프레임 가져와서 원하는 정보만 남겨서 저장
+main_data = pd.read_csv("datatest.csv") # 데이터프레임 형태로 파일 가져옴
+df = pd.DataFrame(main_data,columns=['rank','product_name','date'],index=range(700))
+#메인 데이터에서 필요한 컬럼만 가져와서 저장함. 인덱스 지정해줌(인덱스 지정은 데이터 가공편의를 위해서 함)
 
-#     return rearr
 
-main_data = pd.read_csv("datatest.csv")
-plt.plot(main_data['date'],main_data['rank'],label='product_name')
-plt.xlabel('date')
-plt.ylabel('rank')
-plt.show()
-#데이터 프레임 활용해서 그래프 그리는 영상보고 하면될듯
+#=======================상품이름 배열로 저장하기
+name_df =  pd.DataFrame(df[df.index<=99],columns=['product_name']) 
+#인덱스가 99 보다 작은 애들만 가져와서 인덱스만 이름만 남겨서 데이터 프레임으로 저장함
+name_np = name_df.to_numpy() #데이터 프레임을 넌파이 형식 배열 (2차원 배열로 바꿔줌)
+
+name_arr = [] # 100개의 상품이름만 넣어서 저장할 배열임
+for i in range(100):
+    name_arr.append(name_np[i][0])
+#numpy형식을 배열형태로 저장해줌
+#여기까지 시행시 name_arr에 상품명이 리스트로 저장되어 들어감 (중복없음)
+
+
+#=======================그래프를 그림
+for i in range(100):
+    boxdf = df[(df['product_name'] == name_arr[i])]
+    plt.plot(boxdf['date'],boxdf['rank'])
+#배열안에 저장되어있는 이름으로 값을 가져와서 100개의 데이터를 그래프를 중복해서 그려줌
+
+
+plt.title("DATA SORTING HW")
+plt.xlabel('date') # x축 이름 설정
+plt.ylabel('rank')# y축 이름 설정
+plt.grid(True)
+plt.show()# 그래프 보여줌
