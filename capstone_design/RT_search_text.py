7f17f2527c3804e1b70bf23b39afe9dcb13e6b6c
sangyoungkim <rky7429@gmail.com>
2022-09-21 11:14:26 +0900

    frist commit

--- capstone_design/RT_search_text.py
+++
@@ -1,8 +0,0 @@
-import requests as req 
-import tensorflow
-from bs4 import BeautifulSoup as bfu
-
-signal_RT_search = 'https://signal.bz/'
-address = req.get(signal_RT_search) #브릭 대학원생 채용정보사이트
-address.raise_for_status()
-soup = bfu(address.text, 'html.parser')
