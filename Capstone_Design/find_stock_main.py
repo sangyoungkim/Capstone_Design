cc92658a1f9ce6280648818c73c9caefff93da4c
sangyoungkim <rky7429@gmail.com>
2022-09-25 20:19:34 +0900

    현재작업상환 가독성높여서 프린팅

--- Capstone_Design/find_stock_main.py
+++ Capstone_Design/find_stock_main.py
@@ -3,3 +3,10 @@ import function_group as func
 RT_search_text = func.RT_search_text()
 stock_dic = func.RT_search_stock(RT_search_text)
 a = func.keyword_stock_news(RT_search_text)
+for i in RT_search_text:
+    print(i)
+    print(stock_dic[i])
+    for j in a:
+        if(str(j) == str(i)):
+            print(a[j])
+    print("=====================")
